58a93fd61a4be8a94563bfbd3e88a7f5fff7330d
VivaaindreanNg <123ulugubang@gmail.com>
2022-01-01 23:09:12 +0800

    Completed Problem 20.

--- src/python/problems/problem_20.py
+++ src/python/problems/problem_20.py
@@ -1,2 +1,35 @@
+
+
+def fib(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * fib(n-1)
+
 def solve(n: int) -> int:
-    return 0
+    """
+    Pseudocode:
+    ```
+    fib(n):
+        if n <= 1:
+            return 1
+        else:
+            return n * fib(n-1)
+
+    result = str(fib(n))
+    x := length(result)
+    sum := 0
+
+    for i <- 0 to x-1:
+        sum := sum + int(result[i])
+    
+    return sum
+    ```
+    """
+
+    result = str(fib(n))
+    sum = 0
+    for i in result:
+        sum += int(i)
+
+    return sum
--- src/python/tests/test_20.py
+++ src/python/tests/test_20.py
@@ -8,6 +8,11 @@ from ..problems.problem_20 import solve
 class Test20(unittest.TestCase):
 
     def test(self) -> None:
-        n = 10
-        ans = 3628800
-        self.assertEqual(solve(n), ans)
+        n1 = 10
+        ans1 = 27
+
+        n2 = 100
+        ans2 = 648
+
+        self.assertEqual(solve(n1), ans1)
+        self.assertEqual(solve(n2), ans2)
