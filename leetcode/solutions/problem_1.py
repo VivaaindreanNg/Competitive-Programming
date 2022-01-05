5ab494b009f9b0208e14f7af15ef48b5216024b8
VivaaindreanNg <123ulugubang@gmail.com>
2022-01-06 06:47:09 +0800

    Update Problem 1 with hashmap solution.

--- leetcode/solutions/problem_1.py
+++ leetcode/solutions/problem_1.py
@@ -3,6 +3,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Approach 1: Below works, using 2D array, but TLE.
+        Time complexity: O(n^2)
+
         output = []
 
         n = len(nums)
@@ -15,3 +19,19 @@ class Solution:
                         output.append(j)
 
         return output
+
+
+        Approach 2: Using hashmap. Iterate through all elements
+        only once to find out if there's already value that matches
+        target in the hashmap.
+
+        Time complexity: O(n)
+        """
+
+        hashmap = {}
+        for idx, val in enumerate(nums):
+            if val in hashmap.values():
+                idx_by_existing_val = list(hashmap.values()).index(val)
+                return [idx_by_existing_val, idx]
+            else:
+                hashmap[idx] = target - val
