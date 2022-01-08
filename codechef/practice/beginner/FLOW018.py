e02c34e35c5aa64e3e0bbf057f81a3d45a8111ce
VivaaindreanNg <123ulugubang@gmail.com>
2022-01-09 02:26:35 +0800

    Changed variable & comments for factorial.

--- codechef/practice/beginner/FLOW018.py
+++ codechef/practice/beginner/FLOW018.py
@@ -1,18 +1,18 @@
 class Solution:
     def solve(self, num: int) -> int:
-        # Implement fibonacci with memoization
+        # Implement factorial with memoization
         if num <= 1:
-            fib[num] = 1
+            factorial[num] = 1
         else:
-            fib[num] = num * self.solve(num - 1)
+            factorial[num] = num * self.solve(num - 1)
 
-        return fib[num]
+        return factorial[num]
 
 
 s = Solution()
 t = int(input())
 
 for tc in range(t):
-    fib = [i for i in range(21)]
+    factorial = [i for i in range(21)]
     num = int(input())
     print(s.solve(num))
