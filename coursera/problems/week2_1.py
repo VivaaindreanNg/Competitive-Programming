9828e7b400da959161f3a38d80680d93ecffc549
VivaaindreanNg <123ulugubang@gmail.com>
2022-01-09 22:30:23 +0800

    Updated solutions & test cases with memoization.

--- coursera/problems/week2_1.py
+++ coursera/problems/week2_1.py
@@ -1,12 +1,23 @@
-def solve(num: int) -> int:
+def solve1(num: int) -> int:
     if num == 0:
         return 0
     elif num == 1:
         return 1
     else:
-        return solve(num - 1) + solve(num - 2)
+        return solve1(num - 1) + solve1(num - 2)
 
 
-if __name__ == "__main__":
-    num = int(input())
-    print(solve(num))
+def solve2(num: int) -> int:
+    """With memoization."""
+    fib = [0, 1]
+
+    if num == 0:
+        return fib[0]
+    elif num == 1:
+        return fib[1]
+    else:
+        for n in range(2, num + 1):
+            val = fib[n - 1] + fib[n - 2]
+            fib.append(val)
+
+        return fib[num]
--- coursera/tests/test_week2.py
+++ coursera/tests/test_week2.py
@@ -1,5 +1,6 @@
-from ..problems.week2_1 import solve as solve1
+from ..problems.week2_1 import solve1, solve2
 
+import time
 import unittest
 
 
@@ -13,3 +14,19 @@ class TestWeek2(unittest.TestCase):
 
         self.assertEqual(solve1(num1), ans1)
         self.assertEqual(solve1(num2), ans2)
+
+    def test_week2_1_memoization(self) -> None:
+        num3 = 40
+        ans3 = 102334155
+
+        start1 = time.time()
+        self.assertEqual(solve1(num3), ans3)
+        end1 = time.time()
+        time1 = end1 - start1
+
+        start2 = time.time()
+        self.assertEqual(solve2(num3), ans3)
+        end2 = time.time()
+        time2 = end2 - start2
+
+        self.assertGreater(time1, time2)
